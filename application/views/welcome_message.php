3faa609376908ef3d376dba67b1d00c4ff87e709
WietzeDatachecker <wietze@datachecker.nl>
2014-01-07 15:58:02 +0100

    opzet nvm toets

---
+++ application/views/welcome_message.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	 <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+   	<link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+   	<link rel="stylesheet" href="css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet" media="screen">
+	<title>NVM toets</title>
+
+
+</head>
+<body>
+
+<div id="container">
+<a href="#"> 	
+	<div id="main">
+		<div class="box left">
+			<div class="boxtext"
+				<h1 class="link">business</h1>
+				<h2 class="linktoets ">toets</h2>
+			</div>
+		</div>
+</a>
+<a href="#"> 
+		<div class="box right">
+			<div class="boxtext"
+				<h1 class="link">woon</h1>
+				<h2 class="linktoetsr">toets</h2>
+			</div>
+		</div>
+</a>
+
+		<div class="center_container">
+				<div class="logo">
+					
+					<a href="http://www.nvm.nl" border="0" target="_blank"><img src="images/logo_nvm.jpg" width="127" height="173"  alt="NVM"/></a>
+				</div>
+
+		</div>
+
+		<div class="center_txt">
+			<h1 >Welkom bij NVM toets.nl</h1>
+			
+			<h2>Kies hierboven voor de gewenste toets.</h2>
+		</div>
+	
+	</div>
+
+	
+</div>
+
+</body>
+</html>
